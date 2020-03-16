295a09564c550d00564208e2e96156ada05123d0
github username <github email address>
2020-03-15 17:16:35 -0700

    Added sessions routes and controller.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,8 +11,8 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
 
+    @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created, location: @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
+
+  # sessions route
+  post "api/v1/login", to: "sessions#create"
   namespace :api do
     namespace :v1 do
-      resources :leads 
+      resources :leads
       resources :contacts
-      resources :users 
-    end 
-  end 
-  
-  # namespace :api do 
-  #   namespace :v1 do 
-  #     resources :contacts do 
-  #       resources :leads 
-  #     end 
-  #   end 
-  # end 
-
-  #   namespace :api do
-  #     namespace :v1 do 
-  #       resources :users do 
-  #         resources :leads 
-  #       end 
-  #     end 
-  #   end 
+      resources :users
+    end
+  end
  end
