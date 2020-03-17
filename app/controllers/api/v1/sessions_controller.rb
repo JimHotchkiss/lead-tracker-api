6b51d4ce1d56665d93a78f8ef1c73e4867a7577d
github username <github email address>
2020-03-16 18:44:09 -0700

    Added current_user and logged_in? functionality

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::SessionsController < ApplicationController
 
   def create
+
     @user = User.find_by(username: params[:user][:username])
-    # if @user
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       render json: @user
