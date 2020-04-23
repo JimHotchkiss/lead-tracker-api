35d62134cd5749de812a04aa7e5ca168ebde47b5
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-04-23 15:55:16 -0700

    Added some seed data to test app.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,54 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# users = User.create([{
-#   username: 'User One',
-#   email: 'userOne@gmail.com',
-#   password: 'useronepassword'
+users = User.create([{
+  username: 'User One',
+  email: 'userOne@gmail.com',
+  password: 'useronepassword'
 
-# },
-# {
-#   username: 'User Two',
-#   email: 'userTwo@gmail.com',
-#   password: 'userTwopassword'
-# },
-# {
-#   username: 'User Three',
-#   email: 'userThree@gmail.com',
-#   password: 'userThreepassword'
+},
+{
+  username: 'User Two',
+  email: 'userTwo@gmail.com',
+  password: 'userTwopassword'
+},
+{
+  username: 'User Three',
+  email: 'userThree@gmail.com',
+  password: 'userThreepassword'
 
-# }
-# ])
+}
+])
 
-leads = Lead.create([{
+
+contacts = Contact.create([{
+  name: "Contact One",
+  phone_number: "555-555-55555",
+  email: "oneContact@email.com"
+  },
+  {
+  name: "Contact Two",
+  phone_number: "555-555-55555",
+  email: "twoContact@email.com"
+  },
+  {
+  name: "Contact Three",
+  phone_number: "555-555-55555",
+  email: "threeContact@email.com"
+  },
+  {
+  name: "Contact Four",
+  phone_number: "555-555-55555",
+  email: "fourContact@email.com"
+  },
+  {
+  name: "Contact Five",
+  phone_number: "555-555-55555",
+  email: "fiveContact@email.com"
+  }
+  ])
+
+  leads = Lead.create([{
   product: "Camera",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   urgency: "Medium",
@@ -50,7 +78,7 @@ leads = Lead.create([{
   contact_id: 3
   },
   {
-  product: "Capture Device",
+  product: "Digital Capture",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   urgency: "Low",
   status: "Pending",
@@ -58,38 +86,91 @@ leads = Lead.create([{
   contact_id: 4
   },
   {
-  product: "Camera",
+  product: "Monitor",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   urgency: "High",
   status: "New",
   user_id: 2,
   contact_id: 5
+  },
+  {
+  product: "Camera",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "Medium",
+  status: "New",
+  user_id: 1,
+  contact_id: 1
+  },
+  {
+  product: "Insufflator",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "High",
+  status: "New",
+  user_id: 2,
+  contact_id: 2
+  },
+  {
+  product: "Insufflator",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "High",
+  status: "Open",
+  user_id: 3,
+  contact_id: 3
+  },
+  {
+  product: "Digital Capture",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "Low",
+  status: "Pending",
+  user_id: 3,
+  contact_id: 1
+  },
+  {
+  product: "Monitor",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "High",
+  status: "New",
+  user_id: 2,
+  contact_id: 2
+  },
+  {
+  product: "Camera",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "Medium",
+  status: "New",
+  user_id: 1,
+  contact_id: 3
+  },
+  {
+  product: "Insufflator",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "High",
+  status: "New",
+  user_id: 2,
+  contact_id: 5
+  },
+  {
+  product: "Insufflator",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "High",
+  status: "Open",
+  user_id: 1,
+  contact_id: 1
+  },
+  {
+  product: "Digital Capture",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "Low",
+  status: "Pending",
+  user_id: 1,
+  contact_id: 4
+  },
+  {
+  product: "Monitor",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  urgency: "High",
+  status: "New",
+  user_id: 2,
+  contact_id: 3
   }
 ])
-
-# contacts = Contact.create([{
-#   name: "Contact One",
-#   phone_number: "555-555-55555",
-#   email: "oneContact@email.com"
-#   },
-#   {
-#   name: "Contact Two",
-#   phone_number: "555-555-55555",
-#   email: "twoContact@email.com"
-#   },
-#   {
-#   name: "Contact Three",
-#   phone_number: "555-555-55555",
-#   email: "threeContact@email.com"
-#   },
-#   {
-#   name: "Contact Four",
-#   phone_number: "555-555-55555",
-#   email: "fourContact@email.com"
-#   },
-#   {
-#   name: "Contact Five",
-#   phone_number: "555-555-55555",
-#   email: "fiveContact@email.com"
-#   }
-#   ])
