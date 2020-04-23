d7a171d5875cfad2bed321d93c0f1b940cb40eb6
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-04-22 20:11:50 -0700

    Trying to figure out how to get the user's leads.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(username: params[:user][:username])
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
+      get_user_leads
       render json: @user
     else
       render json: {
@@ -21,13 +22,17 @@ class Api::V1::SessionsController < ApplicationController
 
   def get_current_user
     if logged_in?
-      render json: current_user
+      render json: current_user.leads
     else
       render json: {
         error: "No one is logged in"
       }
     end
   end
+
+  def get_user_leads 
+    binding.pry
+  end 
 
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :username, :email, :leads
-  # has_many :leads 
-  # has_many :contacts, through: :leads
+  # attributes :username, :email, :leads
+  has_many :leads 
+  has_many :contacts, through: :leads
 end
