05b91a39bda4369f5ac91cd3bfdf2d841782cc0f
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-05-14 17:39:06 -0700

    Added user_id and contact_id to the attributes that are retrieved.

--- app/serializers/lead_serializer.rb
+++ app/serializers/lead_serializer.rb
@@ -1,6 +1,6 @@
 class LeadSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :description, :product, :urgency, :status, :contact_email
+  attributes :id, :description, :product, :urgency, :status, :contact_email, :user_id, :contact_id
   belongs_to :user
   belongs_to :contact
 end
