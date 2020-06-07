00777892c98de7b8d0879505fe3b6bf1d786f6eb
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-06-07 16:35:03 -0700

    Building leads#create, and building contact associations.

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::LeadsController < ApplicationController
   before_action :set_lead, only: [:show, :update, :destroy]
+  # before_action :assign_contact, only: [:create]
 
   def index
     @leads = Lead.all
@@ -11,6 +12,16 @@ class Api::V1::LeadsController < ApplicationController
   end
 
   def create
+
+    # user_id === session[:user_id].id
+    # contact_id === contact.id
+
+    @contact = Contact.where("email" => lead_params[:email]).first_or_create do |contact|
+       @contact.name = lead_params[:contact_name]
+       @contact.phone_number = lead_params[:phone_number]
+       @contact.save
+     end 
+     binding.pry
     @lead = Lead.new(lead_params)
 
     if @lead.save
@@ -38,8 +49,17 @@ class Api::V1::LeadsController < ApplicationController
       @lead = Lead.find(params[:id])
     end
 
+    def assign_contact 
+      @contact = Contact.where("email" => params[:lead][:email]).first_or_create do |contact|
+       binding.pry
+       
+        contact.name = params[:lead][:contact_name]
+        contact.phone_number = [:lead][:phone_number]
+        contact.save
+      end 
+    end 
+
     def lead_params
-      binding.pry
-      params.require(:lead).permit(:description, :product, :urgency, :status, :user_id, :contact_id, :contact_email)
+      params.require(:lead).permit(:description, :product, :urgency, :status, :user_id, :contact_id, :contact_name, :email, :phone_number)
     end
 end
