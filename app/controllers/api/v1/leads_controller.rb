af4c5cf042281f5b613fa29dbbebd16f9cd82ac8
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-06-13 16:26:40 -0700

    Trying to get my nested from and my strong params to agree.

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::LeadsController < ApplicationController
   before_action :set_lead, only: [:show, :update, :destroy]
-  # before_action :assign_contact, only: [:create]
 
   def index
     @leads = Lead.all
@@ -12,19 +11,9 @@ class Api::V1::LeadsController < ApplicationController
   end
 
   def create
-
-    # user_id === session[:user_id].id
-    # contact_id === contact.id
-
-    @contact = Contact.where("email" => lead_params[:email]).first_or_create do |contact|
-
-       contact.name = lead_params[:contact_name]
-       contact.phone_number = lead_params[:phone_number]
-       contact.save
-     end 
-     
+    binding.pry
     @lead = Lead.new(lead_params)
-binding.pry
+    binding.pry
     if @lead.save
       render json: @lead, status: :created, location: @lead
     else
@@ -51,6 +40,6 @@ binding.pry
     end 
 
     def lead_params
-      params.require(:lead).permit(:description, :product, :urgency, :status, :user_id, :contact_id, :contact_name, :email, :phone_number)
+      params.require(:lead).permit(:description, :product, :urgency, :status, :user_id, :contact_attributes => [:email, :phone_number, :contact_name])
     end
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,13 @@
 class Lead < ApplicationRecord
   belongs_to :user
   belongs_to :contact
+  accepts_nested_attributes_for :contact
+ 
+
+  # def contact_attributes=(contact_attributes)
+  #   binding.pry
+  #   contact_attributes.each do |contact_attribute|
+  #     self.contact.build(contact_attribute)
+  #   end 
+  # end 
 end
