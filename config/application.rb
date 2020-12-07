29c7960e71355c38a400503eb44642187f72a402
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-12-06 18:13:07 -0800

    Add change to coros

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module LeadTrackerApi
     # CORS
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-          origins 'http://localhost:3001'
+          origins '*'
           resource '*', headers: :any, methods: [:get, :post]
       end
     end
