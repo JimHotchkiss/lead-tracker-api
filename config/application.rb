e0d9c0819e57324033037bb87da967605975d503
Jim Hotchkiss <jimbohotchkiss@yahoo.com>
2020-12-07 10:35:52 -0800

    added client url.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module LeadTrackerApi
     # CORS
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://lead-tracker-api.herokuapp.com/api/v1'
+        origins 'https://lead-tracker-client.herokuapp.com'
         resource '*',
           :headers => :any,
           :methods => [:get, :post, :delete, :put, :patch, :options, :head],
